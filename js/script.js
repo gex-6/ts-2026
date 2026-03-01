00de9fb5dec59399c42b92087bf7b6b6f1b6121f
Taras Stepaniuk <taras.stepanyuk@gmail.com>
2026-03-01 21:07:59 +0200

    hw10

---
+++ js/script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+console.log('Hello JS');
+console.log('Hello JS 2');
+
+// Shift + Option ⌥ + A
+/*  */
+
+const variableName = "John";
+
+ 
+
+/* 
+
+щодо неймінгу змінних
+-> не можна використовувати зарезервовані слова (switch, console, etc.)
+-> 1name -- неможливо починати назву з числа
+-> name i Name -- це будуть різні змінні (case sensisitive)
+-> camelCase
+
+*/
